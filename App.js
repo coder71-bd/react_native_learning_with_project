61520288f6c1998444ec958165b204835a5d3110
coder71 <kamrunnaher877@gmail.com>
2021-12-13 11:33:55 +0600

    react native app initialized

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start with')
+  const changeText = () => {
+    setOutputText('Yaa! The text has been changed.')
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button title='first button' onPress={changeText} />
     </View>
   );
 }
